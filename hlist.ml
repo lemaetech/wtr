9b5ef5e724b32917b5dff4e2236f09d92c0d7fe2
Bikal Lem <gbikal@gmail.com>
2021-04-25 11:11:02 +0100

    rename Witness to Type_witness

--- hlist.ml
+++ hlist.ml
@@ -19,7 +19,8 @@ let hl = Hlist.[ 1; 1.2; "hello" ]
 (* val hl : (int -> float -> string -> void) Hlist.t = Hlist.(::) (1, Hlist.(::)
    (1.2, Hlist.(::) ("hello", Hlist.[]))) *)
 
-module Witness = struct
+(** Type witness for {Hlist}*)
+module Type_witness = struct
   type 'a witness = ..
 
   type 'a witness +=
@@ -32,23 +33,23 @@ module Witness = struct
   end)
 end
 
-let a = Witness.[]
+let a = Type_witness.[]
 (* val a : void Witness.t = Witness.[] *)
 
 (** [pack] packs ['a t] and ['a witness] into a monomorphic type so that we can
     create ['a t] at runtime dynamically. *)
-type pack = Pack : 'a Hlist.t * 'a Witness.t -> pack
+type pack = Pack : 'a Hlist.t * 'a Type_witness.t -> pack
 
 let rec parse : int list -> pack = function
-  | [] -> Pack (Hlist.[], Witness.[])
+  | [] -> Pack (Hlist.[], Type_witness.[])
   | x :: l ->
     let (Pack (v, t)) = parse l in
     if x = 0 then
-      Pack (Hlist.(10. :: v), Witness.(Float :: t))
+      Pack (Hlist.(10. :: v), Type_witness.(Float :: t))
     else if x = 1 then
-      Pack (Hlist.(true :: v), Witness.(Bool :: t))
+      Pack (Hlist.(true :: v), Type_witness.(Bool :: t))
     else
-      Pack (Hlist.(10 :: v), Witness.(Int :: t))
+      Pack (Hlist.(10 :: v), Type_witness.(Int :: t))
 
 (* let route fmt f *)
 (* let match router path *)
