1acf1a7fce0698a4e1c062ecc7d3b4629f42e634
Bikal Lem <gbikal@gmail.com>
2021-04-27 13:28:43 +0100

    merge Path and Path_pattern module

--- router.ml
+++ router.ml
@@ -8,6 +8,24 @@ module Path = struct
     | Int : ('a, 'b) t -> (int -> 'a, 'b) t
     | Float : ('a, 'b) t -> (float -> 'a, 'b) t
     | Bool : ('a, 'b) t -> (bool -> 'a, 'b) t
+
+  (** Path pattern - Monomorphic version of [t]. *)
+  type p_pattern =
+    | PLiteral : string -> p_pattern
+    | PString : p_pattern
+    | PInt : p_pattern
+    | PFloat : p_pattern
+    | PBool : p_pattern
+
+  (* [of_path path] converts [path] to [Path_pattern.t list]. This is done to
+     get around some typing issue with using Path.t in the [add] function below. *)
+  let rec pattern : type a b. (a, b) t -> p_pattern list = function
+    | End -> []
+    | Literal (lit, path) -> PLiteral lit :: pattern path
+    | String path -> PString :: pattern path
+    | Int path -> PInt :: pattern path
+    | Float path -> PFloat :: pattern path
+    | Bool path -> PBool :: pattern path
 end
 
 type 'c route = Route : ('a, 'c) Path.t * 'a -> 'c route
@@ -15,7 +33,7 @@ type 'c route = Route : ('a, 'c) Path.t * 'a -> 'c route
 (** ['a t] is a trie based router where ['a] is the route value. *)
 type 'a t =
   | Node of
-      { route : 'a option
+      { route : 'a route option
       ; literal : 'a t String.Map.t
       ; int_param : 'a t option
       }
@@ -28,32 +46,16 @@ let ( @-> ) : ('a, 'b) Path.t -> 'a -> 'b route = fun path f -> Route (path, f)
 
 let empty = empty_with None
 
-module Path_pattern = struct
-  type t =
-    | Literal of string
-    | String
-    | Int
-    | Float
-    | Bool
+module Path_pattern = struct end
 
-  (* [of_path path] converts [path] to [Path_pattern.t list]. This is done to
-     get around some typing issue with using Path.t in the [add] function below. *)
-  let rec of_path : type a b. (a, b) Path.t -> t list = function
-    | Path.End -> []
-    | Literal (lit, path) -> Literal lit :: of_path path
-    | String path -> String :: of_path path
-    | Int path -> Int :: of_path path
-    | Float path -> Float :: of_path path
-    | Bool path -> Bool :: of_path path
-end
-
-let add : 'b route -> 'b route t -> 'b route t =
+let add : 'b route -> 'b t -> 'b t =
  fun route t ->
   let (Route (path, _)) = route in
-  let path_patterns = Path_pattern.of_path path in
-  let rec loop (Node t) = function
+  let path_patterns = Path.pattern path in
+  let rec loop : 'b t -> Path.p_pattern list -> 'b t =
+   fun (Node t) -> function
     | [] -> Node { t with route = Some route }
-    | Path_pattern.Literal lit :: path_patterns ->
+    | PLiteral lit :: path_patterns ->
       let literal =
         match String.Map.find t.literal lit with
         | Some t' ->
@@ -65,7 +67,7 @@ let add : 'b route -> 'b route t -> 'b route t =
           String.Map.add_exn t.literal ~key:lit ~data:(loop empty path_patterns)
       in
       Node { t with literal }
-    | Int :: path_patterns ->
+    | PInt :: path_patterns ->
       let int_param =
         let t' = Option.value t.int_param ~default:empty in
         Some (loop t' path_patterns)
@@ -74,6 +76,8 @@ let add : 'b route -> 'b route t -> 'b route t =
     | _ -> assert false
   in
   loop t path_patterns
+
+(* let match': 'b route t -> string -> 'b option = *)
 
 let r1 =
   Path.(String (Int End)) @-> fun (s : string) (i : int) -> s ^ string_of_int i
