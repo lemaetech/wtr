a69d06d52fcfb39376530c48abe0dcec133cd1e2
Bikal Lem <gbikal@gmail.com>
2021-04-28 10:51:29 +0100

    rename path to uri and param to var

--- router.ml
+++ router.ml
@@ -1,73 +1,74 @@
 open! Core
 
-type ('a, 'b) path =
-  | End : ('b, 'b) path
-  | Literal : string * ('a, 'b) path -> ('a, 'b) path
-      (** Literal path component eg. 'home' in '/home' *)
-  | Param : 'c param * ('a, 'b) path -> ('c -> 'a, 'b) path
-      (** Parameter path component eg. ':int' in '/home/:int' *)
+(** [('a, 'b) uri] represents a uniform resource identifier *)
+type ('a, 'b) uri =
+  | End : ('b, 'b) uri
+  | Literal : string * ('a, 'b) uri -> ('a, 'b) uri
+      (** Literal uri component eg. 'home' in '/home' *)
+  | Var : 'c var * ('a, 'b) uri -> ('c -> 'a, 'b) uri
+      (** Uri variable component, i.e. the value is determined during runtime,
+          eg. ':int' in '/home/:int' *)
 
 (* Parameter detail. *)
-and 'c param =
+and 'c var =
   { decode : string -> 'c option
   ; encode : 'c -> string
   ; name : string (* name e.g. :int, :float, :bool, :string etc *)
   }
 
-let end_ : ('b, 'b) path = End
+let end_ : ('b, 'b) uri = End
 
-let lit : string -> ('a, 'b) path -> ('a, 'b) path =
- fun s path -> Literal (s, path)
+let lit : string -> ('a, 'b) uri -> ('a, 'b) uri = fun s uri -> Literal (s, uri)
 
-let param decode encode name path = Param ({ encode; decode; name }, path)
+let var decode encode name uri = Var ({ encode; decode; name }, uri)
 
-let string : ('a, 'b) path -> (string -> 'a, 'b) path =
- fun path -> param (fun s -> Some s) Fun.id ":string" path
+let string : ('a, 'b) uri -> (string -> 'a, 'b) uri =
+ fun uri -> var (fun s -> Some s) Fun.id ":string" uri
 
-let int : ('a, 'b) path -> (int -> 'a, 'b) path =
- fun path -> param int_of_string_opt string_of_int ":int" path
+let int : ('a, 'b) uri -> (int -> 'a, 'b) uri =
+ fun uri -> var int_of_string_opt string_of_int ":int" uri
 
-let float : ('a, 'b) path -> (float -> 'a, 'b) path =
- fun path -> param float_of_string_opt string_of_float ":float" path
+let float : ('a, 'b) uri -> (float -> 'a, 'b) uri =
+ fun uri -> var float_of_string_opt string_of_float ":float" uri
 
-let bool : ('a, 'b) path -> (bool -> 'a, 'b) path =
- fun path -> param bool_of_string_opt string_of_bool ":bool" path
+let bool : ('a, 'b) uri -> (bool -> 'a, 'b) uri =
+ fun uri -> var bool_of_string_opt string_of_bool ":bool" uri
 
-(** [kind] encodes path kind/type. *)
+(** [kind] encodes uri component kind/type. *)
 type kind =
   | KLiteral : string -> kind
-  | KParam : 'c param -> kind
+  | KVar : 'c var -> kind
 
-(* [kind path] converts [path] to [kind list]. This is done to get around OCaml
-   type inference issue when using [path] type in the [add] function below. *)
-let rec kind : type a b. (a, b) path -> kind list = function
+(* [kind uri] converts [uri] to [kind list]. This is done to get around OCaml
+   type inference issue when using [uri] type in the [add] function below. *)
+let rec kind : type a b. (a, b) uri -> kind list = function
   | End -> []
-  | Literal (lit, path) -> KLiteral lit :: kind path
-  | Param (conv, path) -> KParam conv :: kind path
+  | Literal (lit, uri) -> KLiteral lit :: kind uri
+  | Var (conv, uri) -> KVar conv :: kind uri
 
-(** ['c route] is a path and its handler. ['c] represents the value returned by
+(** ['c route] is a uri and its handler. ['c] represents the value returned by
     the handler. *)
-type 'c route = Route : ('a, 'c) path * 'a -> 'c route
+type 'c route = Route : ('a, 'c) uri * 'a -> 'c route
 
-(** [p @-> route_handler] creates a route from path [p] and [route_handler]. *)
-let ( @-> ) : ('a, 'b) path -> 'a -> 'b route = fun path f -> Route (path, f)
+(** [p @-> route_handler] creates a route from uri [p] and [route_handler]. *)
+let ( @-> ) : ('a, 'b) uri -> 'a -> 'b route = fun uri f -> Route (uri, f)
 
 (** ['a t] is a trie based router where ['a] is the route value. *)
 type 'a t =
   | Node of
       { route : 'a route option
       ; literals : 'a t String.Map.t
-      ; params : 'a t String.Map.t
+      ; vars : 'a t String.Map.t
       }
 
 let empty_with route =
-  Node { route; literals = String.Map.empty; params = String.Map.empty }
+  Node { route; literals = String.Map.empty; vars = String.Map.empty }
 
 let empty = empty_with None
 
 let add : 'b route -> 'b t -> 'b t =
  fun route t ->
-  let (Route (path, _)) = route in
+  let (Route (uri, _)) = route in
   let rec loop : 'b t -> kind list -> 'b t =
    fun (Node t) kinds ->
     let add_update m key kinds =
@@ -84,20 +85,20 @@ let add : 'b route -> 'b t -> 'b t =
     | KLiteral lit :: kinds ->
       let literals = add_update t.literals lit kinds in
       Node { t with literals }
-    | KParam p :: kinds ->
-      let params = add_update t.params p.name kinds in
-      Node { t with params }
+    | KVar p :: kinds ->
+      let vars = add_update t.vars p.name kinds in
+      Node { t with vars }
   in
-  loop t (kind path)
+  loop t (kind uri)
 
-let rec apply : type a b. (a, b) path -> a -> string list -> b =
- fun path f params ->
-  match (path, params) with
+let rec apply : type a b. (a, b) uri -> a -> string list -> b =
+ fun uri f vars ->
+  match (uri, vars) with
   | End, [] -> f
-  | Literal (_, path), params -> apply path f params
-  | Param (conv, path), p :: params -> (
+  | Literal (_, uri), vars -> apply uri f vars
+  | Var (conv, uri), p :: vars -> (
     match conv.decode p with
-    | Some p' -> apply path (f p') params
+    | Some p' -> apply uri (f p') vars
     | None -> failwith "Route not matched")
   | _, _ -> failwith "Route not matched"
 
@@ -109,9 +110,9 @@ let match' : 'b t -> string -> 'b option =
       | _ -> false)
     |> String.split ~on:'/'
   in
-  let loop (Node t) params tokens =
+  let loop (Node t) vars tokens =
     match tokens with
-    | [] -> Option.map t.route ~f:(fun (Route (path, f)) -> apply path f params)
+    | [] -> Option.map t.route ~f:(fun (Route (path, f)) -> apply path f vars)
     | _ -> assert false
   in
   loop t [] tokens
@@ -128,8 +129,8 @@ let r3 : string route =
 let r4 : string route =
   lit "home" (float end_) @-> fun (f : float) -> string_of_float f
 
-(** This should give error (we added an extra () param in handler) but it
-    doesn't. It only errors when adding to the router.*)
+(** This should give error (we added an extra () var in handler) but it doesn't.
+    It only errors when adding to the router.*)
 let r5 =
   string (int end_) @-> fun (s : string) (i : int) () -> s ^ string_of_int i
 
