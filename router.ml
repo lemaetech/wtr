9233483e2945aa18cdac9297e7259d864a8cfc47
Bikal Lem <gbikal@gmail.com>
2021-05-06 15:46:31 +0100

    ensure whichever uri path component added first is evaluated first

--- router.ml
+++ router.ml
@@ -1,7 +1,7 @@
 open! Core
 
 (** Variable type. *)
-module Vty = struct
+module Var_ty = struct
   type _ t = ..
 
   type _ t += Int : int t | Float : float t | Bool : bool t | String : string t
@@ -31,7 +31,7 @@ type ('a, 'b) uri =
 and 'c var =
   { decode : string -> 'c option
   ; name : string (* name e.g. int, float, bool, string etc *)
-  ; tid : 'c Vty.t
+  ; tid : 'c Var_ty.t
   }
 
 let end_ : ('b, 'b) uri = End
@@ -41,31 +41,28 @@ let lit : string -> ('a, 'b) uri -> ('a, 'b) uri = fun s uri -> Literal (s, uri)
 let var decode name tid uri = Var ({ decode; name; tid }, uri)
 
 let string : ('a, 'b) uri -> (string -> 'a, 'b) uri =
- fun uri -> var (fun s -> Some s) "string" Vty.String uri
+ fun uri -> var (fun s -> Some s) "string" Var_ty.String uri
 
 let int : ('a, 'b) uri -> (int -> 'a, 'b) uri =
- fun uri -> var int_of_string_opt "int" Vty.Int uri
+ fun uri -> var int_of_string_opt "int" Var_ty.Int uri
 
 let float : ('a, 'b) uri -> (float -> 'a, 'b) uri =
- fun uri -> var float_of_string_opt "float" Vty.Float uri
+ fun uri -> var float_of_string_opt "float" Var_ty.Float uri
 
 let bool : ('a, 'b) uri -> (bool -> 'a, 'b) uri =
- fun uri -> var bool_of_string_opt "bool" Vty.Bool uri
+ fun uri -> var bool_of_string_opt "bool" Var_ty.Bool uri
 
 (** [uri_kind] Existential which encodes uri kind/type. *)
 type uri_kind =
   | KLiteral : string -> uri_kind
-  | KVar : kvar -> uri_kind
-
-(** 'c var uri kind existential. *)
-and kvar = KV : 'c var -> kvar
+  | KVar : 'c var -> uri_kind
 
 (* [kind uri] converts [uri] to [kind list]. This is done to get around OCaml
    type inference issue when using [uri] type in the [add] function below. *)
 let rec uri_kind : type a b. (a, b) uri -> uri_kind list = function
   | End -> []
   | Literal (lit, uri) -> KLiteral lit :: uri_kind uri
-  | Var (var, uri) -> KVar (KV var) :: uri_kind uri
+  | Var (var, uri) -> KVar var :: uri_kind uri
 
 (** ['c route] is a uri and its handler. ['c] represents the value returned by
     the handler. *)
@@ -76,38 +73,37 @@ let ( >- ) : ('a, 'b) uri -> 'a -> 'b route = fun uri f -> Route (uri, f)
 
 (** ['a t] is a node in a trie based router. *)
 type 'a t =
-  { route : 'a route option
-  ; literals : 'a t String.Map.t
-  ; vars : (kvar * 'a t) Queue.t
+  { route : 'a route option (* ; literals : 'a t String.Map.t *)
+  ; path : (uri_kind * 'a t) Queue.t
   }
 
-let empty_with route =
-  { route; literals = String.Map.empty; vars = Queue.create () }
+let empty_with route = { route; path = Queue.create () }
 
 let empty = empty_with None
 
-let add : 'b route -> 'b t -> 'b t =
- fun route t ->
-  let (Route (uri, _)) = route in
+let add (Route (uri, _) as route) t =
   let rec loop : 'b t -> uri_kind list -> 'b t =
    fun t uri_kinds ->
     match uri_kinds with
     | [] -> { t with route = Some route }
-    | KLiteral lit :: uri_kinds ->
-      let literals =
-        String.Map.change t.literals lit ~f:(function
-          | Some t' -> Some (loop t' uri_kinds)
-          | None -> Some (loop empty uri_kinds))
-      in
-      { t with literals }
-    | KVar kvar :: uri_kinds ->
-      let (KV var) = kvar in
-      (Queue.find t.vars ~f:(fun (KV var', _) ->
-           String.equal var.name var'.name)
-      |> function
-      | Some (kvar, t') -> Queue.enqueue t.vars (kvar, loop t' uri_kinds)
-      | None -> Queue.enqueue t.vars (kvar, loop empty uri_kinds));
-      t
+    | (KLiteral lit as kvar) :: uri_kinds ->
+      update_path
+        ~f:(function
+          | KLiteral lit', _ -> String.equal lit lit'
+          | _ -> false)
+        t kvar uri_kinds
+    | (KVar var as kvar) :: uri_kinds ->
+      update_path
+        ~f:(function
+          | KVar var', _ -> String.equal var.name var'.name
+          | _ -> false)
+        t kvar uri_kinds
+  and update_path ~f t kvar uri_kinds =
+    (Queue.find t.path ~f
+    |> function
+    | Some (kvar, t') -> Queue.enqueue t.path (kvar, loop t' uri_kinds)
+    | None -> Queue.enqueue t.path (kvar, loop empty uri_kinds));
+    t
   in
   loop t (uri_kind uri)
 
@@ -120,21 +116,22 @@ let rec match' : 'b t -> string -> 'b option =
     | [] ->
       Option.map t.route ~f:(fun (Route (uri, f)) ->
           exec_route_handler f (uri, List.rev decoded_values))
-    | uri_token :: uri_tokens -> (
-      (* Check if one of the vars are matched first. If none is matched then
-         match literals. The route that is added first is evaluated first. *)
-      Queue.fold_until t.vars ~init:None
-        ~f:(fun _ (KV var, t') ->
-          match var.decode uri_token with
-          | Some v -> Stop (Some (D (var, v), t'))
-          | None -> Continue None)
+    | uri_token :: uri_tokens ->
+      Queue.fold_until t.path ~init:None
+        ~f:
+          (fun _ -> function
+            | KVar var, t' -> (
+              match var.decode uri_token with
+              | Some v -> Stop (Some (D (var, v) :: decoded_values, t'))
+              | None -> Continue None)
+            | KLiteral lit, t' ->
+              if String.equal lit uri_token then
+                Stop (Some (decoded_values, t'))
+              else
+                Continue None)
         ~finish:(fun _ -> None)
-      |> function
-      | Some (value, t') ->
-        (loop [@tailcall]) t' (value :: decoded_values) uri_tokens
-      | None ->
-        Option.bind (String.Map.find t.literals uri_token) ~f:(fun t' ->
-            (loop [@tailcall]) t' decoded_values uri_tokens))
+      |> Option.bind ~f:(fun (decoded_values, t') ->
+             (loop [@tailcall]) t' decoded_values uri_tokens)
   in
   let uri_tokens =
     String.rstrip uri ~drop:(function
@@ -151,8 +148,8 @@ and exec_route_handler : type a b. a -> (a, b) uri * decoded_value list -> b =
   | Literal (_, uri), decoded_values ->
     exec_route_handler f (uri, decoded_values)
   | Var ({ tid; _ }, uri), D ({ tid = tid'; _ }, v) :: decoded_values -> (
-    match Vty.eq tid tid' with
-    | Some Vty.Eq -> exec_route_handler (f v) (uri, decoded_values)
+    match Var_ty.eq tid tid' with
+    | Some Var_ty.Eq -> exec_route_handler (f v) (uri, decoded_values)
     | None -> assert false)
   | _, _ -> assert false
 
