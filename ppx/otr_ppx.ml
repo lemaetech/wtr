c3d997017b5a9f17e9e9febbfbc1d083597db285
Bikal Lem <gbikal@gmail.com>
2021-05-09 20:08:41 +0100

    use Result monad

--- ppx/otr_ppx.ml
+++ ppx/otr_ppx.ml
@@ -1,43 +1,64 @@
 open Ppxlib
-module A = Ast_builder.Default
+module Ast_builder = Ast_builder.Default
+
+let ( let* ) r f = Result.bind r f
+
+let ( >>= ) = ( let* )
+
+let ( let+ ) r f = Result.map f r
+
+let validate_path uri =
+  let valid_start_token uri =
+    let path = Uri.path uri |> String.split_on_char '/' in
+    match List.hd path with
+    | "" -> Ok (List.tl path)
+    | _
+    | (exception _) ->
+      Error "otr: path specification must start with '/'"
+  in
+  let valid_end_token path =
+    let trailing_slash_count =
+      List.fold_left
+        (fun acc a ->
+          if String.equal a "" then
+            acc + 1
+          else
+            acc)
+        0 path
+    in
+    if trailing_slash_count > 1 then
+      Error
+        "otr: Invalid uri path specification. Only one trailing '/' is allowed."
+    else
+      Ok path
+  in
+  valid_start_token uri >>= valid_end_token
+
+let validate_query uri =
+  let exception E of string in
+  try
+    let query_comps =
+      Uri.query uri
+      |> List.map (fun (k, v) ->
+             if List.length v != 1 then
+               raise
+                 (E (Printf.sprintf "otr: invalid query specification %s." k))
+             else
+               [ k; List.hd v ])
+      |> List.concat
+    in
+    Ok query_comps
+  with
+  | E msg -> Error msg
 
 let rec parse ~loc otr =
-  let uri = Uri.of_string otr in
-  let path = Uri.path uri |> String.split_on_char '/' in
-  if starts_with_slash path then
-    path_components ~loc path @ query_components ~loc uri |> otr_expression ~loc
-  else
-    Location.raise_errorf ~loc "Otr path must start with '/'"
-
-and starts_with_slash path =
-  try List.hd path = "" with
-  | _ -> false
-
-and path_components ~loc path =
-  let path = List.tl path in
-  let trailing_slash_count =
-    List.fold_left
-      (fun acc a ->
-        if String.equal a "" then
-          acc + 1
-        else
-          acc)
-      0 path
-  in
-  if trailing_slash_count > 1 then
-    Location.raise_errorf ~loc
-      "otr: Invalid uri path specification. Only one trailing '/' is allowed."
-  else
-    path
-
-and query_components ~loc uri =
-  Uri.query uri
-  |> List.map (fun (k, v) ->
-         if List.length v != 1 then
-           Location.raise_errorf ~loc "otr: invalid query specification %s." k
-         else
-           [ k; List.hd v ])
-  |> List.concat
+  (let uri = Uri.of_string otr in
+   let* path_components = validate_path uri in
+   let+ query_components = validate_query uri in
+   path_components @ query_components)
+  |> function
+  | Ok otr_tokens -> otr_expression ~loc otr_tokens
+  | Error msg -> Location.raise_errorf ~loc "otr: %s" msg
 
 and otr_expression ~loc = function
   | [] -> [%expr Otr.Private.nil]
@@ -65,12 +86,13 @@ and otr_expression ~loc = function
       let longident_loc = { txt = Longident.parse custom_arg; loc } in
       [%expr
         Otr.Private.arg
-          [%e A.pexp_ident ~loc longident_loc]
+          [%e Ast_builder.pexp_ident ~loc longident_loc]
           [%e otr_expression ~loc components]]
     | x -> Location.raise_errorf ~loc "otr: unrecognized component: %s." x)
   | comp :: components ->
     [%expr
-      Otr.Private.lit [%e A.estring ~loc comp]
+      Otr.Private.lit
+        [%e Ast_builder.estring ~loc comp]
         [%e otr_expression ~loc components]]
 
 let extend ~loc ~path:_ otr =
