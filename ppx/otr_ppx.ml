20e9dbda9dd21e780f67719c2747e9db08d52c72
Bikal Lem <gbikal@gmail.com>
2021-05-09 20:16:01 +0100

    rename to decode

--- ppx/otr_ppx.ml
+++ ppx/otr_ppx.ml
@@ -7,14 +7,29 @@ let ( >>= ) = ( let* )
 
 let ( let+ ) r f = Result.map f r
 
-let validate_path uri =
+let rec decode_otr_expression ~loc otr =
+  (let* uri = decode_uri otr in
+   let* path_components = decode_path_tokens uri in
+   let+ query_components = decode_query_tokens uri in
+   path_components @ query_components)
+  |> function
+  | Ok otr_tokens -> otr_expression ~loc otr_tokens
+  | Error msg -> Location.raise_errorf ~loc "otr: %s" msg
+
+and decode_uri otr =
+  if String.trim otr |> String.length > 0 then
+    Ok (Uri.of_string otr)
+  else
+    Error "Path is empty"
+
+and decode_path_tokens uri =
   let valid_start_token uri =
     let path = Uri.path uri |> String.split_on_char '/' in
     match List.hd path with
     | "" -> Ok (List.tl path)
     | _
     | (exception _) ->
-      Error "otr: path specification must start with '/'"
+      Error "Path specification must start with '/'"
   in
   let valid_end_token path =
     let trailing_slash_count =
@@ -27,38 +42,25 @@ let validate_path uri =
         0 path
     in
     if trailing_slash_count > 1 then
-      Error
-        "otr: Invalid uri path specification. Only one trailing '/' is allowed."
+      Error "Invalid uri path specification. Only one trailing '/' is allowed"
     else
       Ok path
   in
   valid_start_token uri >>= valid_end_token
 
-let validate_query uri =
+and decode_query_tokens uri =
   let exception E of string in
   try
-    let query_comps =
-      Uri.query uri
-      |> List.map (fun (k, v) ->
-             if List.length v != 1 then
-               raise
-                 (E (Printf.sprintf "otr: invalid query specification %s." k))
-             else
-               [ k; List.hd v ])
-      |> List.concat
-    in
-    Ok query_comps
+    Uri.query uri
+    |> List.map (fun (k, v) ->
+           if List.length v != 1 then
+             raise (E (Printf.sprintf "Invalid query specification %s" k))
+           else
+             [ k; List.hd v ])
+    |> List.concat
+    |> Result.ok
   with
   | E msg -> Error msg
-
-let rec parse ~loc otr =
-  (let uri = Uri.of_string otr in
-   let* path_components = validate_path uri in
-   let+ query_components = validate_query uri in
-   path_components @ query_components)
-  |> function
-  | Ok otr_tokens -> otr_expression ~loc otr_tokens
-  | Error msg -> Location.raise_errorf ~loc "otr: %s" msg
 
 and otr_expression ~loc = function
   | [] -> [%expr Otr.Private.nil]
@@ -88,24 +90,20 @@ and otr_expression ~loc = function
         Otr.Private.arg
           [%e Ast_builder.pexp_ident ~loc longident_loc]
           [%e otr_expression ~loc components]]
-    | x -> Location.raise_errorf ~loc "otr: unrecognized component: %s." x)
+    | x -> Location.raise_errorf ~loc "otr: Unrecognized component: %s" x)
   | comp :: components ->
     [%expr
       Otr.Private.lit
         [%e Ast_builder.estring ~loc comp]
         [%e otr_expression ~loc components]]
 
-let extend ~loc ~path:_ otr =
-  if String.trim otr |> String.length > 0 then
-    parse ~loc otr
-  else
-    Location.raise_errorf ~loc "otr: invalid path specification."
+let extend ~loc ~path:_ otr = decode_otr_expression ~loc otr
 
-let name = "otr"
+let ppx_name = "otr"
 
 let ext =
-  Extension.declare name Extension.Context.Expression
+  Extension.declare ppx_name Extension.Context.Expression
     Ast_pattern.(single_expr_payload (estring __))
     extend
 
-let () = Driver.register_transformation name ~extensions:[ ext ]
+let () = Driver.register_transformation ppx_name ~extensions:[ ext ]
