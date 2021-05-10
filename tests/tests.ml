d1995672c5f3f9fcd57d5da0cec5825e916ccc53
Bikal Lem <gbikal@gmail.com>
2021-05-10 01:09:55 +0100

    add tests for :string and :bool

--- tests/tests.ml
+++ tests/tests.ml
@@ -4,14 +4,21 @@ let router =
   Otr.(
     create
       [ {%otr| /home/about |} >- "about page"
-      ; ([%otr "/home/:int/"]
+      ; ({%otr| /home/:int/ |}
         >- fun i -> "Product Page. Product Id : " ^ string_of_int i)
-      ; ([%otr "/home/:float/"]
+      ; ({%otr| /home/:float/ |}
         >- fun f -> "Float page. number : " ^ string_of_float f)
-      ; ([%otr "/contact/*/:int"]
+      ; ({%otr| /contact/*/:int |}
         >- fun name number ->
         "Contact page. Hi, " ^ name ^ ". Number " ^ string_of_int number)
       ; {%otr| /home/products/** |} >- "full splat page"
+      ; ({%otr| /home/*/** |} >- fun s -> "Wildcard page. " ^ s)
+      ; ({%otr| /contact/:string/:bool |}
+        >- fun name call_me_later ->
+        "Contact Page2. Name "
+        ^ name
+        ^ ". Call me later: "
+        ^ string_of_bool call_me_later)
       ])
 
 let () =
@@ -29,4 +36,11 @@ let () =
     = match' router "/home/products/asdfasdf\nasdfasdfasd");
   assert (Some "full splat page" = match' router "/home/products/");
   assert (None = match' router "/home/products");
-  assert (None = match' router "/home/")
+  assert (Some "Wildcard page. product1" = match' router "/home/product1/");
+  assert (None = match' router "/home/product1");
+  assert (
+    Some "Contact Page2. Name bikal. Call me later: true"
+    = match' router "/contact/bikal/true");
+  assert (
+    Some "Contact Page2. Name bob. Call me later: false"
+    = match' router "/contact/bob/false")
