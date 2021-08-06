968310cfaf2ea8ce02f23e3ad82db90f6d90ad7b
Bikal Lem <gbikal@gmail.com>
2021-08-06 15:49:11 +0100

    fix doc formatting

--- lib/wtr.mli
+++ lib/wtr.mli
@@ -135,21 +135,21 @@ val match' : method' -> string -> 'a t -> 'a option
     The match call results in the following results:
 
     {v
-     ====Router Match Results====
-      1: Float page. number : 100001.1
-      2: Int page. number : 100001
-      3: about page
-      4: Product1 dyson350. Id: 233. q = true
-      5: Product1 dyson350. Id: 2. q = false
-      6: Product2 dyson350. Id: 2.
-      7: None
-      8: Apples are juicy!
-      9: Orange is a citrus fruit.
-     10: Pineapple has scaly skin
-     11: None
-     12: FAQ page for category : products
-     13: FAQ page for category : products
-     14: FAQ page for category : insurance
+====Router Match Results====
+ 1: Float page. number : 100001.1
+ 2: Int page. number : 100001
+ 3: about page
+ 4: Product1 dyson350. Id: 233. q = true
+ 5: Product1 dyson350. Id: 2. q = false
+ 6: Product2 dyson350. Id: 2.
+ 7: None
+ 8: Apples are juicy!
+ 9: Orange is a citrus fruit.
+10: Pineapple has scaly skin
+11: None
+12: FAQ page for category : products
+13: FAQ page for category : products
+14: FAQ page for category : insurance
     v} *)
 
 (** {1:uri Specifying a URI}
