09c4bd3be187682bd9162e9e46b8ef0c27fcdbb6
Bikal Lem <gbikal+git@gmail.com>
2021-10-20 17:23:14 +0100

    add nixos support

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "WTR NixOS development environment using flakes";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs-unstable, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs-unstable.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            gmp
+            libev
+          ];
+        };
+      });
+}
